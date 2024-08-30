a6c3de0ff94a29ea42a44472c5c46fe57b8e6ea3
Jiri Velek <bleksak@protonmail.com>
2024-08-30 16:25:08 +0200

    add basic support for multiline strings

--- grammar.js
+++ grammar.js
@@ -1,109 +1,74 @@
 module.exports = grammar({
-    name: 'neon',
-    // TODO: add multiline strings
-    externals: $ => [
-        $.indent,
-        $.dedent,
-        $.newline,
-    ],
-    rules: {
-        document: $ => repeat($.property),
+	name: "neon",
+	// TODO: add multiline strings
+	externals: ($) => [$.indent, $.dedent, $.newline],
+	rules: {
+		document: ($) => repeat($.property),
 
-        property: $ => prec.left(2, choice(
-            $.comment,
-            seq(
-                $.lvalue,
-                ':',
-                choice(
-                    $.newline,
-                    seq(
-                        $.indent,
-                        repeat1($.property),
-                        $.dedent,
-                    ),
-                    $.rvalue,
-                ),
-                optional($.comment),
-            ),
-        )),
+		property: ($) =>
+			prec.left(
+				2,
+				choice(
+					$.comment,
+					seq(
+						$.lvalue,
+						":",
+						choice(
+							$.newline,
+							seq($.indent, repeat1($.property), $.dedent),
+							$.rvalue,
+						),
+						optional($.comment),
+					),
+				),
+			),
 
-        lvalue: $ => choice(
-            $.identifier_lvalue,
-            $.number,
-            $.string,
-        ),
+		lvalue: ($) => choice($.identifier_lvalue, $.number, $.string),
 
-        rvalue: $ => choice(
-            $.identifier,
-            $.string,
-            $.number,
-            $.boolean,
-            $.entity,
-            $.array,
-            $.list,
-        ),
+		rvalue: ($) =>
+			choice(
+				$.identifier,
+				$.string,
+				$.number,
+				$.boolean,
+				$.entity,
+				$.array,
+				$.list,
+			),
 
-        entity: $ => seq(
-            $.identifier,
-            '(',
-            optional($.param_list),
-            ')'
-        ),
+		entity: ($) => seq($.identifier, "(", optional($.param_list), ")"),
 
-        param_list: $ => seq(
-            $.property,
-            repeat(
-                seq(
-                    ',',
-                    $.parameter
-                )
-            ),
-            optional(','),
-        ),
+		param_list: ($) =>
+			seq($.property, repeat(seq(",", $.parameter)), optional(",")),
 
-        parameter: $ => choice(
-            $.rvalue,
-            seq(
-                $.identifier,
-                ':',
-                $.rvalue
-            )
-        ),
+		parameter: ($) => choice($.rvalue, seq($.identifier, ":", $.rvalue)),
 
-        array: $ => seq(
-            '[',
-            optional(seq($.rvalue, repeat(seq(',', $.rvalue)))),
-            ']'
-        ),
+		array: ($) =>
+			seq("[", optional(seq($.rvalue, repeat(seq(",", $.rvalue)))), "]"),
 
-        list: $ => prec.left(2,
-            seq(
-                $.indent,
-                repeat1(
-                    seq($.list_item)
-                ),
-                $.dedent,
-            )
-        ),
+		list: ($) =>
+			prec.left(2, seq($.indent, repeat1(seq($.list_item)), $.dedent)),
 
-        list_item: $ => seq(
-            '-',
-            $.rvalue,
-        ),
+		list_item: ($) => seq("-", $.rvalue),
 
-        identifier: () => /[a-zA-Z_\d\*\\\.\%][ \w\.\*\\\:\%]*/,
-        // same as identifier, but allow numbers even at the beginning
-        identifier_lvalue: () => /[a-zA-Z_\d\*\\][\w\.\*\\]*/,
-        string: () => /".*" | \'.*\'/,
-        octal_number: () => /0o[0-7]+/,
-        hex_number: () => /0x[0-9a-f]+/,
-        exponential_number: () => /[+\-]?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+\-]?\d+)?/,
-        number: $ => choice(/\d+(\.\d+)?/, $.octal_number, $.hex_number, $.exponential_number),
-        boolean: () => choice("true", "false"),
-        comment: $ => seq(
-            '#',
-            $.unquoted_string,
-        ),
-        unquoted_string: () => /[^\n]+/,
-    }
+		identifier: () => /[a-zA-Z_\d\*\\\.\%][ \w\.\*\\\:\%]*/,
+		// same as identifier, but allow numbers even at the beginning
+		identifier_lvalue: () => /[a-zA-Z_\d\*\\][\w\.\*\\]*/,
+		string: ($) =>
+			choice(
+                seq(/\'\'\'/, repeat(choice(/./, $.newline, $.indent, $.dedent)), /\'\'\'/),
+                seq(/\"\"\"/, repeat(choice(/./, $.newline, $.indent, $.dedent)), /\"\"\"/),
+				/".*"/,
+				/\'.*\'/,
+			),
+		octal_number: () => /0o[0-7]+/,
+		hex_number: () => /0x[0-9a-f]+/,
+		exponential_number: () =>
+			/[+\-]?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+\-]?\d+)?/,
+		number: ($) =>
+			choice(/\d+(\.\d+)?/, $.octal_number, $.hex_number, $.exponential_number),
+		boolean: () => choice("true", "false"),
+		comment: ($) => seq("#", $.unquoted_string),
+		unquoted_string: () => /[^\n]+/,
+	},
 });
