b00df463a83c9d7ece09e3a4b4084e318e622f02
Jiri Velek <bleksak@protonmail.com>
2024-08-30 20:09:34 +0200

    fix: add slash to allowed identifiers

--- grammar.js
+++ grammar.js
@@ -53,7 +53,7 @@ module.exports = grammar({
 
 		list_item: ($) => seq("-", $.rvalue),
 
-		identifier: () => /[a-zA-Z_\d\*\\\.\%][ \w\.\*\\\:\%]*/,
+		identifier: () => /[a-zA-Z_\d\*\\\.\%\/][\/ \w\.\*\\\:\%]*/,
 		// same as identifier, but allow numbers even at the beginning
 		identifier_lvalue: () => /[a-zA-Z_\d\*\\][\w\.\*\\]*/,
 		string: ($) =>
